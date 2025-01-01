be662a00f84964cc7b1951cad9b37f90a955665c
AdrianGerman <adrian.german1019@gmail.com>
2025-01-01 08:34:55 -0600

    home component

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,9 +1,12 @@
 import "./App.css"
+import { Home } from "./components/Home"
 
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <main className="flex justify-center items-center">
+        <Home />
+      </main>
     </>
   )
 }
---
+++ web/src/components/Home.jsx
@@ -0,0 +1,26 @@
+import { Github } from "./icons/Github"
+
+export const Home = () => {
+  return (
+    <>
+      <section className="flex justify-center items-center flex-col w-2/3 gap-6 mt-60">
+        <h1 className="text-6xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#883aea] via-[#f4edfc] to-[#ffff]">
+          Heim components
+        </h1>
+        <p>
+          Bienvenido a este catalogo de componentes de css y tailwind, espero
+          puedas encontrar algo de tu agrado, estoy abierto a sugerencias!
+        </p>
+        <a
+          href="https://github.com/AdrianGerman/templates-heim"
+          target="_blank"
+          rel="noopener"
+          className="inline-flex items-center justify-center px-2 py-1 text-sm font-semibold text-center text-white bg-black/50 border border-black rounded-lg md:px-4 md:py-2 md:text-sm transform transition duration-300 hover:border-gray-500 hover:scale-105"
+        >
+          <Github />
+          Repositorio
+        </a>
+      </section>
+    </>
+  )
+}
