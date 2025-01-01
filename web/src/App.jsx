69295bab814dca01f6140a1f85cf8ea7f95f09fd
AdrianGerman <adrian.german1019@gmail.com>
2025-01-01 09:19:21 -0600

    routes and eslint rules

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,6 @@
 import Home from "./components/Home"
 import Sidebar from "./components/Sidebar"
+import Cards from "./components/pages/Cards"
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
@@ -9,7 +10,10 @@ function App() {
       <div className="flex h-screen">
         <Sidebar />
         <main className="flex-1 overflow-y-auto p-6">
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cards" element={<Cards />} />
+          </Routes>
         </main>
       </div>
     </Router>
