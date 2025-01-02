767b24809299a7532031e280f3d6693a7e5f4051
AdrianGerman <adrian.german1019@gmail.com>
2025-01-02 05:55:23 -0600

    new styles in card component

--- web/src/components/pages/Cards.jsx
+++ web/src/components/pages/Cards.jsx
@@ -1,29 +1,47 @@
-import React from "react"
 import { components } from "../../data/components"
-import { Link } from "react-router-dom"
 
 const Cards = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold mb-4">Cards</h1>
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+    <div className="p-6">
+      <h1 className="text-4xl font-bold text-center text-gray-200 mb-8">
+        Cards
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {components.cards.map((component, index) => (
-          <div key={index} className="bg-[#282828] shadow-lg p-4 rounded">
-            <h2 className="text-lg font-semibold">{component.name}</h2>
+          <div
+            key={index}
+            className="bg-gray-800 shadow-lg p-6 rounded-lg hover:shadow-2xl transform hover:-translate-y-1 transition duration-300"
+          >
+            <h2 className="text-2xl font-semibold text-gray-100 mb-4">
+              {component.name}
+            </h2>
+
             <img
               src={component.image}
               alt={component.name}
-              className="w-full h-48 object-cover mb-4 rounded"
+              className="w-full h-48 object-cover rounded-lg mb-4"
             />
-            <p>{component.description}</p>
-            <a
-              target="_blank"
-              href={component.url}
-              rel="noopener noreferrer"
-              className="inline-block px-4 py-2 text-sm font-medium text-center text-white bg-blue-600 rounded hover:bg-blue-700"
-            >
-              Ver archivo
-            </a>
+
+            <p className="text-gray-300 mb-6">{component.description}</p>
+
+            <div className="flex justify-between">
+              <a
+                href={component.fileUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="px-4 py-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 transition"
+              >
+                Previsualizar
+              </a>
+              <a
+                href={component.codeUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="px-4 py-2 text-sm font-medium text-center text-white bg-gray-600 rounded-lg hover:bg-gray-700 transition"
+              >
+                Código
+              </a>
+            </div>
           </div>
         ))}
       </div>
--- web/src/data/components.jsx
+++ web/src/data/components.jsx
@@ -5,8 +5,10 @@ export const components = {
     {
       name: "Card Simple",
       description: "Un componente de tarjeta simple.",
-      image: "/projects/01.webp",
-      url: "/projects/simple-card.html",
+      image: "/projects/cards/01.webp",
+      fileUrl: "/projects/cards/simple-card.html",
+      codeUrl:
+        "https://github.com/AdrianGerman/templates-heim/blob/main/web/public/projects/simple-card.html",
       render: () => <div className="p-4 border rounded">Simple card</div>
     }
   ],
