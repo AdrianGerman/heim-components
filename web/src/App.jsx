21d6ec1eb7d124fc4cddfdd993cf1277fb3163dd
AdrianGerman <adrian.german1019@gmail.com>
2025-01-03 07:53:22 -0600

    hide/show sidebar

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,15 +1,30 @@
+import React, { useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./components/Home"
 import Sidebar from "./components/Sidebar"
 import Cards from "./components/pages/Cards"
 import Images from "./components/pages/Images"
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { Menu } from "./components/icons/Menu"
 
 function App() {
+  const [isSidebarVisible, setSidebarVisible] = useState(true)
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!isSidebarVisible)
+  }
+
   return (
     <Router>
       <div className="flex h-screen">
-        <Sidebar />
+        {isSidebarVisible && <Sidebar toggleSidebar={toggleSidebar} />}
+
+        {!isSidebarVisible && (
+          <button onClick={toggleSidebar} className="fixed top-4 left-4 z-50">
+            <Menu />
+          </button>
+        )}
+
         <main className="flex-1 overflow-y-auto p-6">
           <Routes>
             <Route path="/" element={<Home />} />
--- web/src/components/Sidebar.jsx
+++ web/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { Menu } from "./icons/Menu"
 
 const sections = [
   { name: "Home", path: "/" },
@@ -6,10 +7,15 @@ const sections = [
   { name: "Images", path: "/images" }
 ]
 
-const Sidebar = () => {
+const Sidebar = ({ toggleSidebar }) => {
   return (
-    <nav className="w-64 bg-[#171717] text-white h-full p-4">
-      <h2 className="text-xl font-bold mb-6">Secciones</h2>
+    <nav className="w-64 bg-[#171717] text-white h-full p-4 relative shadow-lg">
+      <div className="flex justify-between items-center mb-6">
+        <h2 className="text-xl font-bold">Secciones</h2>
+        <button className="" onClick={toggleSidebar}>
+          <Menu />
+        </button>
+      </div>
       <ul className="space-y-2">
         {sections.map((section) => (
           <li key={section.name}>
