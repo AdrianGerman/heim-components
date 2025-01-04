cdde350073bbe12c2e2aa1f5bad40922338b205b
AdrianGerman <adrian.german1019@gmail.com>
2025-01-04 10:16:52 -0600

    code url

--- web/src/data/components.jsx
+++ web/src/data/components.jsx
@@ -7,7 +7,7 @@ export const components = {
       description: "Un componente de tarjeta simple.",
       image: "/projects/cards/01.webp",
       fileUrl: "/projects/cards/simple-card.html",
-      codeUrl: ""
+      codeUrl: "/examples/cards/simple-card.html"
     }
   ],
   images: [
@@ -16,14 +16,14 @@ export const components = {
       description: "Galería de imágenes con efecto vertical de scroll.",
       image: "/projects/images/02.webp",
       fileUrl: "/projects/images/image-scroll.html",
-      codeUrl: ""
+      codeUrl: "/example/images/image-scroll.html"
     },
     {
       name: "Image gallery vertical",
       description: "Galería de imagines vertical con transiciones.",
       image: "/projects/images/03.webp",
       fileUrl: "/projects/images/image-gallery-vertical.html",
-      codeUrl: ""
+      codeUrl: "/example/images/image-gallery-vertical.html"
     },
     {
       name: "Image smart shadow",
@@ -31,7 +31,7 @@ export const components = {
         "Fondo de imagen personalizado en base a la imagen principal.",
       image: "/projects/images/04.webp",
       fileUrl: "/projects/images/smart-shadow.html",
-      codeUrl: ""
+      codeUrl: "/example/images/smart-shadow.html"
     }
   ],
   loaders: [
@@ -40,7 +40,7 @@ export const components = {
       description: "Un loader circular simple.",
       image: "/projects/loaders/05.webp",
       fileUrl: "/projects/loaders/spinner.html",
-      codeUrl: ""
+      codeUrl: "/example/loaders/spinner.html"
     }
   ]
 }
